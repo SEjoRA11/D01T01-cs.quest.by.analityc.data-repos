009c33becff7f51a1dcb23651405f2b1c69c06b5
SEjoRA1! <alt_ser1607@mail.ru>
2025-12-23 13:31:18 +0700

    fir quest_1 and relis

---
+++ MyApp/quest_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int NMAX = 100;
+        int[,] B = new int[NMAX, NMAX];
+        Console.WriteLine("get N: ");
+        int n = int.Parse(Console.ReadLine()!);
+        int num = 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                B[i, j] = num;
+                num++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(B[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
